b304d14e321fdeb84622f40d525d0a421d0c8c04
An <an@mail.ru>
2022-08-10 22:04:18 +0300

    вторая задача, первого дз

---
+++ homework01/Ex02/programm02.cs
@@ -0,0 +1,19 @@
+﻿int a=26;
+int b=36;
+int c=10;
+
+int max=a;
+int min=b;
+
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(a<min) min=a;
+if(b<min) min=b;
+if(c<min) min=c;
+
+Console.Write("max=");
+Console.WriteLine(max);
+
+Console.Write("min=");
+Console.WriteLine(min);
