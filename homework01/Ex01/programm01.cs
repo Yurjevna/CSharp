5e74a30d2330589815bf50c4de0241eed9060161
An <an@mail.ru>
2022-08-10 21:53:33 +0300

    первая задача, первого дз

---
+++ homework01/Ex01/programm01.cs
@@ -0,0 +1,16 @@
+﻿int a=26;
+int b=36;
+
+int max=a;
+int min=b;
+
+if(a>max) max=a;
+if(a<min) min=a;
+if(b>max) max=b;
+if(b<min) min=b;
+
+Console.Write("max=");
+Console.WriteLine(max);
+
+Console.Write("min=");
+Console.WriteLine(min);
