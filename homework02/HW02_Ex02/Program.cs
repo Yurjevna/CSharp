f83f067690058c4b7daca793a01b12750a73b9cf
An <an@mail.ru>
2022-08-14 06:56:42 +0300

    2задача 2й домашки

---
+++ homework02/HW02_Ex02/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string s = Convert.ToString(a);
+if (s.Length > 2)
+{
+    Console.WriteLine(s[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
