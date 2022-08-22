62f099b9c2d0393d75744d197646ac3406e1b8c5
An <an@mail.ru>
2022-08-22 21:29:17 +0300

    1задача дз4

---
+++ homework04/Ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int Step(int A, int B)
+{
+    int n= A;
+    for(int i=1; i<B; i++)
+    {
+        n=n*A;
+    }
+    return n;
+}
+int result=Step(3,5);
+Console.WriteLine(result);
