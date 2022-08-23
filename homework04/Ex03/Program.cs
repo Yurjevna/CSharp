967810801900903ff3655bdafdf1b9a75b52d9a3
An <an@mail.ru>
2022-08-23 21:08:52 +0300

    3задача дз4

---
+++ homework04/Ex03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] array = new int[8];
+for(int i=0; i<array.Length; i++)
+{
+  Console.Write($"Введите элемент массива под индексом {i}:\t");
+  array[i]=int.Parse(Console.ReadLine());
+}
+Console.WriteLine ("Вывод массива: ");
+for(int i=0; i<array.Length; i++)
+{
+  Console.Write(array[i]);
+}
