8a998908d448ec52549b74542a507511c19319db
An <an@mail.ru>
2022-08-24 21:05:17 +0300

    5 дз

---
+++ homework05/HW05_Ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] array = new int [4];
+for (int i = 0; i < array.Length; i++)
+{
+array[i]= new Random().Next(1,20);
+Console.WriteLine("Значение элемента массива " + i +  " = "  + array[i]);
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 !=0)
+    {
+        
+        sum =sum + array[i];
+    }
+}
+Console.Write("Сумма нечетных элементов массива ");
+Console.Write(sum);
+
+
+
+
+
+
+
+    
---
+++ homework05/HW05_Ex03/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между 
+//максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int[] array = { 3, 7, 22, 2, 78 };
+int minValue = array[0];
+int maxValue = array[0];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < minValue)
+    {
+        minValue = array[i];
+    }
+    if (array[i] > maxValue)
+    {
+        maxValue = array[i];
+    }
+    
+}
+Console.WriteLine(minValue);
+Console.WriteLine(maxValue);
+
+int result = maxValue - minValue;
+Console.WriteLine(result);
