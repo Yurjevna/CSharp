a70acc413297796d6ef3b0d6f0bdce9f82624919
An <an@mail.ru>
2022-08-28 09:18:42 +0300

    1 задача 6 дз

---
+++ homework06/HW06_Ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int []array = new int [5];
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}: ");
+    array[i]= int.Parse(Console.ReadLine());
+}
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i]>0)
+    {
+     count=count+1;
+    }
+}
+Console.WriteLine("Количество положительных элементов массива: ");
+Console.Write(count);
---
+++ homework06/HW06_Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Metod(int b1; int k1; int b2; int k2);
+{
+    int x;
+    int y;
+    if (k1 != K2) ;
+    {
+        x = (b1 - b2) * (k1 - k2);
+        y = k1 * ((b2 - b1) / (k1 - k2) + b1);
+        return x;
+        return y;
+    }
+    else (k1 - k2);
+    {
+        return 0;
+    }
+}
+int result = Metod(2, 5, 4, 9);
+Console.WriteLine(result);
