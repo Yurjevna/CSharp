3c9e4ca6039ba278c386f6a8911d96d40a168438
An <an@mail.ru>
2022-09-03 07:21:15 +0300

    8 дз

---
+++ homework08/HW08_Ex01/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+
+void WriteArray(int[,] array)
+Сизов Иван
+Сегодня, 21:36
+Кому:вам
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    array [i, j] = Convert.ToInt32(new Random().Next(0,21));
+}
+Console.WriteLine();
+ 
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();
+ 
+WriteArray (array);
+ 
+void Range (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int change = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = change;
+                }
+            }
+         }
+    }
+}    
+ 
+Range(array);
+ 
+Console.WriteLine();
+ 
+WriteArray (array);
+
---
+++ homework08/HW08_Ex02/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int m = 3;
+int n = 4;
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    array [i, j] = Convert.ToInt32(new Random().Next(0,21));
+}
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    Console.Write(array[i,j] + "\t ");
+    Console.WriteLine();
+}
+Console.WriteLine();
+ 
+int SumLineEl (int[,] array, int i)
+{
+    int SumLine = array [i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        SumLine += array[i, j];
+    }
+    return SumLine;
+}
+ 
+int MinSumLines = SumLineEl(array, 0);
+int CurSumeLine = 0;
+int IndMinSumLine = 0;
+for(int i = 1; i < array.GetLength(0); i++)
+{
+    CurSumeLine = SumLineEl(array, i);
+    if(CurSumeLine < MinSumLines)
+    {
+        MinSumLines = CurSumeLine;
+        IndMinSumLine = i;
+    }
+}
+ 
+Console.Write("Индекс строки с наименьшей суммой элементов: ");
+Console.WriteLine(IndMinSumLine);
+ 
