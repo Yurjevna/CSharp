feeaa4310c09ad647241a39315e68717baff4d63
An <an@mail.ru>
2022-09-09 22:35:13 +0300

    9 дз

---
+++ homework09/Program.cs
@@ -0,0 +1,14 @@
+﻿int Over (int M, int N)
+{
+    int sum = 0;
+if (M>N)
+    for (int i = N; i <= M; i++)
+    sum = sum + i;
+    else
+        for (int i = M; i <= N; i++)
+        sum = sum + i;
+        return sum;
+}
+int result = Over (2,8);
+Console.Write ("Итого сумма чисел составляет: ");
+Console.WriteLine(result);
